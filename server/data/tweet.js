4c1e287f46c880c4ecbbf0ce1c2b7226919eb0bb
bella <103259907+WolgyeBella@users.noreply.github.com>
2024-10-29 00:06:02 +0900

    refactor: mvc 패턴으로 리팩토링

---
+++ server/data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+  {
+    id: "1",
+    text: "개발공부 화이팅!",
+    createAt: Date.now().toString(),
+    name: "Bella",
+    username: "bella",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+  {
+    id: "2",
+    text: "Hello everyone!",
+    createAt: Date.now().toString(),
+    name: "Ellie",
+    username: "ellie",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+];
+
+export async function getAll() {
+  return tweets;
+}
+
+export async function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export async function getById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export async function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export async function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export async function remove(id) {
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+}
